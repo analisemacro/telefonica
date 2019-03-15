3ef5d9d2dea7eae9b0091cb86b99a2b44e9a7dfe
Repositório Análise Macro <macroeconomia@vitorwilher.com>
2019-03-15 10:49:35 -0300

    Add files via upload

--- Dia05/churn.R
+++ Dia05/churn.R
@@ -32,3 +32,20 @@ for(i in 1:ncol(churn[,cols_recode1])) {
                                         (churn[,cols_recode1][,i], from =c("No internet service"),to=c("No")))
 }
 
+## Verificar o tenure
+min(churn$tenure); max(churn$tenure)
+
+group_tenure <- function(tenure){
+  if (tenure >= 0 & tenure <= 12){
+    return('0-12 Month')
+  }else if(tenure > 12 & tenure <= 24){
+    return('12-24 Month')
+  }else if (tenure > 24 & tenure <= 48){
+    return('24-48 Month')
+  }else if (tenure > 48 & tenure <=60){
+    return('48-60 Month')
+  }else if (tenure > 60){
+    return('> 60 Month')
+  }
+}
+
